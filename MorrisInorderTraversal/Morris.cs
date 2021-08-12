599d50c9848dad1db8398b4a154cc51fb421a942
Farnoud Taghdissi Borhani <Farnood_t@yahoo.com>
2021-08-11 18:30:05 -0700

    Add project files.

---
+++ MorrisInorderTraversal/Morris.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorrisInorderTraversal
+{
+    class Morris
+    {
+        public void MorrisTraversal(Node root)
+        {
+            Node current, pre;
+
+            if (root == null)
+                return;
+
+            current = root;
+            while (current != null)
+            {
+                if (current.left == null)
+                {
+                    Console.Write(current.data + " ");
+                    current = current.right;
+                }
+                else
+                {
+                    pre = current.left;
+                    while (pre.right != null && pre.right != current)
+                        pre = pre.right;
+
+                    if (pre.right == null)
+                    {
+                        pre.right = current;
+                        current = current.left;
+                    }
+                    else
+                    {
+                        pre.right = null;
+                        Console.Write(current.data + " ");
+                        current = current.right;
+                    } 
+                } /* End of if condition current->left == NULL*/
+
+            } /* End of while */
+        }
+    }
+}
---
+++ MorrisInorderTraversal/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MorrisInorderTraversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node tree = new Node(20);
+            tree.left = new Node(10);
+            tree.left.left = new Node(5);
+            tree.left.right = new Node(15);
+
+            tree.right = new Node(30);
+            tree.right.left = new Node(25);
+            tree.right.right = new Node(35);
+
+            Morris M = new Morris();
+            M.MorrisTraversal(tree);
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
